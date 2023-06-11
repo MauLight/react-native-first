2b801c2c414fd648fb799cf9788f21092e6381dc
MauLight <maulisseluz@gmail.com>
2023-06-11 19:17:55 -0400

    image placeholder added

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import ImageViewer from './components/ImageViewer';
+
+const PlaceHolderImage = require('./assets/images/background-image.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hey Elv! I created a mobile app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource= {PlaceHolderImage}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    color: '#white',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18
+  }
 });
+
+const styles2 = StyleSheet.create({
+  container: {
+    color: '#fff',
+  }
+})
